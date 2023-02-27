2f5647b5a6c30de0d91785ef6b491b7d900c31bb
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-27 15:53:48 +0300

    add NumberOfPage class

---
+++ src/main/java/by/itacademy/nikolaykrysiuk/javabasics/NumberOfPages.java
@@ -0,0 +1,17 @@
+package by.itacademy.nikolaykrysiuk.javabasics;
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+public class NumberOfPages {
+    public static void main(String[] args) {
+        int numbers, total;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество новостей ");
+        numbers = sc.nextInt();
+        if(numbers < 10){
+            System.out.println("Данное количество новостей помещается на одной странице");
+        }else {
+            total = numbers / 10;
+            System.out.println("Для данного количства новостей необходимо: " + (total + 1) + " страниц");
+        }
+    }
+}
