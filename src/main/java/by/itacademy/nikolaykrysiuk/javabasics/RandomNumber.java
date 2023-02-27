284394a0a483ff258af108a45cb7e2071db99e2e
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-27 15:58:45 +0300

    add RandonNumber class

---
+++ src/main/java/by/itacademy/nikolaykrysiuk/javabasics/RandomNumber.java
@@ -0,0 +1,8 @@
+package by.itacademy.nikolaykrysiuk.javabasics;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        int number = (int) (Math.random() * 100);
+        System.out.println(number);
+    }
+}
