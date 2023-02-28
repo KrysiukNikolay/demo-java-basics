73d4725b19963339305c57f04a28d2b7e26b6366
KrysiukNikolay <Kolian888@yandex.ru>
2023-02-28 11:08:39 +0300

    resactor Reverse class

--- src/main/java/by/itacademy/nikolaykrysiuk/javabasics/Reverse.java
+++ src/main/java/by/itacademy/nikolaykrysiuk/javabasics/Reverse.java
@@ -1,8 +1,20 @@
 package by.itacademy.nikolaykrysiuk.javabasics;
+import java.util.Scanner;
+import java.sql.Array;
+
 public class Reverse {
     public static void main(String[] args) {
-        for(int i = args.length; i >= 0; i--) {
-            System.out.println("Argument " + i + " = " + args[i-1]);
+        int lenght;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество элементов");
+        lenght = sc.nextInt();
+        int[] array = new int[lenght];
+        System.out.println(" Введите " + lenght + " чисел");
+        for(int j = 0; j<array.length;j++ ){
+            array[j] = sc.nextInt();
+        }
+        for(int i = array.length; i > 0; i--) {
+            System.out.println("Argument " + i + " = " + array[i-1]);
         }
     }
 }
